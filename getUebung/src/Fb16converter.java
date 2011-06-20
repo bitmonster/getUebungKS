394ee6d392c4ea31bb993abb5bcbbd5acdb543fe
Johannes S <Johannes.scheuch@googlemail.com>
2011-06-20 19:02:55 +0200

    ne name

---
+++ getUebung/src/Fb16converter.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+ 
+
+public class Fb16converter(){
+    
+    
+}
